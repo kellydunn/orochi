d5e81a4886a292463417c610824e014cdbc89383
kellydunn <defaultstring@gmail.com>
2011-01-11 21:57:49 -0800

    [instance methods] Added stubs

--- lib/acts_as_routeable/instance_methods.rb
+++ lib/acts_as_routeable/instance_methods.rb
@@ -1,4 +1,12 @@
 module ActsAsRouteable::InstanceMethods
   # has_many routes or has_one route? hmmm
   # instance methods go here!
+
+  # TODO return constructed directions_json
+  def directions
+  end
+
+  # TODO Return constrcuted polyline_json
+  def path
+  end
 end
