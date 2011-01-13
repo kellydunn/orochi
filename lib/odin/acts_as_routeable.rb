5000ccf6922140e096caa8eef69f194105640b7c
kellydunn <defaultstring@gmail.com>
2011-01-13 13:10:53 -0800

    [acts_as] Working on routing relationship

--- lib/odin/acts_as_routeable.rb
+++ lib/odin/acts_as_routeable.rb
@@ -8,11 +8,15 @@ module Odin
       def acts_as_routeable(options = {})
         metaclass = (class << self; self; end)
 
+        # Relationships to make routing work
+        belongs_to :route
+
         send :extend, Odin::ActsAsRouteable::InstanceMethods 
       end
     end
     
     module InstanceMethods
+      
     end
   end
 end
