de07a1c81de38c4ef8f8227957171fbb7a23c470
kellydunn <defaultstring@gmail.com>
2011-01-13 14:03:38 -0800

    [models][generators] Crosses fingers >___<

--- lib/generators/odin/models_generator.rb
+++ lib/generators/odin/models_generator.rb
@@ -2,6 +2,11 @@ require "rails/generators"
 
 class ModelsGenerator < Rails::Generators::Base
   def create_models
-    # TODO create all templates/models
+    dir_path = File.join(File.dirname(__FILE__), 'templates/models')
+    Dir.new(dir_path).each do |file|
+      if !File.directory?(file)
+        template file
+      end
+    end
   end
 end
