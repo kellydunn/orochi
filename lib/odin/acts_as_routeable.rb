e16f457281e08938a96ae219bd548ed16dffe568
kellydunn <defaultstring@gmail.com>
2011-01-13 12:28:47 -0800

    [acts_as] Continuing to follow geokit's example

--- lib/odin/acts_as_routeable.rb
+++ lib/odin/acts_as_routeable.rb
@@ -6,6 +6,11 @@ module Odin
     end
     
     module ClassMethods
+      def acts_as_routeable(options = {})
+        metaclass = (class << self; self; end)
+
+        send :extend, Odin::ActsAsRouteable::InstanceMethods 
+      end
     end
     
     module InstanceMethods
