280e170ef8732ed4289303fcd30f2b7df57af5f1
kellydunn <defaultstring@gmail.com>
2011-01-13 13:16:33 -0800

    [lib] Removing unncessary code in base file

--- lib/odin.rb
+++ lib/odin.rb
@@ -1,11 +1,7 @@
 require "active_model"
 require "active_record"
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
 require "odin/acts_as_routeable"
-
-$LOAD_PATH.shift
 
 if defined?(ActiveRecord::Base)
   Odin::ActsAsRouteable.include ActiveRecord::Base
