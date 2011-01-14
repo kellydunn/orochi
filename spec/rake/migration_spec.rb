164545d2310197bc74fc2d024729f46ed35e0ebe
kellydunn <defaultstring@gmail.com>
2011-01-14 12:44:02 -0800

    [spec] DRYing up spec code

--- spec/rake/migration_spec.rb
+++ spec/rake/migration_spec.rb
@@ -3,9 +3,15 @@ require 'spec_helper'
 describe "Migration rake" do
   include CoreHelper 
 
+  before :each do
+    create_rails_app
+  end
+
+  after :each do
+    delete_rails_app
+  end
+
   it "should create all necessary migrations" do
-    Dir.chdir("spec/staging")
-    system("rails new gem_test -d mysql -m ../../lib/rails_templates/default_template.rb")
     success = Dir.new("gem_test/db/migrate/").all? do |file|
       File.basename(file).match("odin_migration")
     end
