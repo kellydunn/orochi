63f2b54beab50c047ab07f4b37705e02ba965c71
kellydunn <defaultstring@gmail.com>
2011-01-14 09:23:42 -0800

    [acts_as] Refining some base functionality

--- lib/odin/acts_as_routeable.rb
+++ lib/odin/acts_as_routeable.rb
@@ -14,6 +14,10 @@ module Odin
         # TODO if you have many routes, then
         # we need to have a foreign key of metaclass.class.id
         has_many :routes
+
+        # Just for starters
+        self.start = options[:start].nil?? "San Francisco, CA" : options[:start]
+        self.start = options[:start].nil?? "Seattle, WA" : options[:start]
         
         send :extend, Odin::ActsAsRouteable::InstanceMethods 
       end
@@ -23,13 +27,11 @@ module Odin
     
       #TODO FUUUU I need to simplify this from an OOP perspective
       def find_gdirections
-=begin
         request_str = "http://maps.googleapis.com/maps/api/directions/json?sensor=false&alternatives=true&"
         request_str += "origin=#{CGI::escape(self.start)}&"
         request_str += "destination=#{CGI::escape(self.stop)}"
         response = open(request_str)
         return JSON.parse(response.read)
-=end
       end
 
       # TODO reformat this so that we're
@@ -38,18 +40,35 @@ module Odin
       def populate_polylines
         json = find_gdirections
         json_routes = json["routes"]
+        
+        # for each route
         json_routes.each do |route|
-          polyline = []
+          
+          # Create new route
+
+          # for each leg
           route["legs"].each do |route_leg|
+
+            # Create new leg
+
+            # for each step
             route_leg["steps"].each do |leg_step|
               leg_points = leg_step["polyline"]["points"]
               leg_levels = leg_step["polyline"]["levels"]
+
+              # decode polyline for points
+              # with res level
               polyline_data = GoogleMapsPolyline.decode_polyline(leg_points, leg_levels)
+
+              # push polyline information
+              polyline = []
               polyline_data.collect do |point|
                 polyline.push([point[0], point[1]])
               end
+
+              # create step
+              Step.create!()
             end
-            polylines.create(:path_json => polyline.inspect)
           end
         end
       end
