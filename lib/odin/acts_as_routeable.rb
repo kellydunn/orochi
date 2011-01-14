af2037c8b2530f25dbf205e399f01cc944b84450
kellydunn <defaultstring@gmail.com>
2011-01-13 16:35:35 -0800

    [model][acts_as] Slowly integrating my old rerouter functionality

--- lib/odin/acts_as_routeable.rb
+++ lib/odin/acts_as_routeable.rb
@@ -1,3 +1,6 @@
+require 'net/https'
+require 'open-uri'
+
 module Odin
   module ActsAsRouteable
     def self.include(base)
@@ -11,14 +14,49 @@ module Odin
         # Relationships to make routing work
         belongs_to :route
         
-        
-
         send :extend, Odin::ActsAsRouteable::InstanceMethods 
       end
     end
     
     module InstanceMethods
-      
+    
+      #TODO FUUUU I need to simplify this from an OOP perspective
+      def find_gdirections
+=begin
+        request_str = "http://maps.googleapis.com/maps/api/directions/json?sensor=false&alternatives=true&"
+        request_str += "origin=#{CGI::escape(self.start)}&"
+        request_str += "destination=#{CGI::escape(self.stop)}"
+        response = open(request_str)
+        return JSON.parse(response.read)
+=end
+      end
+
+      # TODO reformat this so that we're
+      # talking to the same models inside of our gem
+      # generated database
+      def populate_polylines
+=begin
+        json = find_gdirections
+        json_routes = json["routes"]
+        json_routes.each do |route|
+          
+          # Creating a polyline with each step in the
+          # polyline's points
+          polyline = []
+          route["legs"].each do |route_leg|
+            route_leg["steps"].each do |leg_step|
+              leg_points = leg_step["polyline"]["points"]
+              leg_levels = leg_step["polyline"]["levels"]
+              polyline_data = GoogleMapsPolyline.decode_polyline(leg_points, leg_levels)
+              polyline_data.collect do |point|
+                polyline.push([point[0], point[1]])
+              end
+            end
+            polylines.create(:path_json => polyline.inspect)
+          end
+        end
+=end
+      end
     end
   end
 end
