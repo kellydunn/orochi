41286d278f0e45b480f5cc83a2c682ef7a727949
kellydunn <defaultstring@gmail.com>
2011-01-17 15:32:51 -0800

    [spec] Adding naiive helper methods for spec

--- spec/helpers/core.rb
+++ spec/helpers/core.rb
@@ -16,5 +16,22 @@ module CoreHelper
       puts "Was not able to delete rails application"
     end
   end
+
+  def mock_rails_env
+    database_yml = File.expand_path('../database.yml', __FILE__)
+    if File.exists?(database_yml)
+      active_record_configuration = YAML.load_file(database_yml)["mysql"]
+      
+      ActiveRecord::Base.establish_connection(active_record_configuration)
+      ActiveRecord::Base.logger = Logger.new(File.join(File.dirname(__FILE__), "debug.log"))
+      
+      ActiveRecord::Base.silence do
+        ActiveRecord::Migration.verbose = false
+        
+        load(File.dirname(__FILE__) + '/schema.rb')
+        load(File.dirname(__FILE__) + '/models.rb')
+      end
+    end
+  end
 end
 
